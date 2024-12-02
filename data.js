fea9b29b1f81171e0f70f0f3c451c68c29837c6a
komingshyu <komingshyu@yahoo.com.tw>
2024-12-02 15:34:35 +0800

    新增程式

---
+++ data.js
@@ -0,0 +1,10 @@
+const linksData = [
+    { name: "1A2B猜數字遊戲", description: "傳統1A2B猜數字遊戲", link: "1A2B猜數字遊戲.html" },
+    { name: "2048遊戲", description: "好玩的二進位遊戲", link: "2048-game.html" },
+    { name: "圖像智慧拼接系統", description: "將連續拍攝的照片拼接成一張完整圖像，需要有30%-50%的重疊影像", link: "photo_stitching/index.html" },
+    { name: "圖片自動裁切程式", description: "將圖像依照橫向縱向數量自動分割，並可微調分割線", link: "photo_auto_cut.html" },
+    { name: "批次獎狀套印", description: "可載入A4獎狀圖像及得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "批次獎狀套印(150dpi).html" },
+    { name: "單張獎狀套印", description: "可載入A4獎狀圖像及輸入得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "單張獎狀套印(150dpi).html" },
+    { name: "A4三角桌牌產生器", description: "修改1張A4可以產生2張桌牌，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器.html" },
+
+];
