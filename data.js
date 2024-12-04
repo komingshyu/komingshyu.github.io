51027f8a804bf29842b779738494e382527b4cc0
komingshyu <komingshyu@yahoo.com.tw>
2024-12-04 14:01:37 +0800

    三角桌牌增加色彩及背景

--- data.js
+++ data.js
@@ -5,6 +5,7 @@ const linksData = [
     { name: "圖片自動裁切程式", description: "將圖像依照橫向縱向數量自動分割，並可微調分割線", link: "photo_auto_cut.html" },
     { name: "批次獎狀套印", description: "可載入A4獎狀圖像及得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "批次獎狀套印(150dpi).html" },
     { name: "單張獎狀套印", description: "可載入A4獎狀圖像及輸入得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "單張獎狀套印(150dpi).html" },
-    { name: "A4三角桌牌產生器", description: "修改1張A4可以產生2張桌牌，並記錄座標定位，改為直接開啟PDF，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器03.html" },
+    { name: "A4三角桌牌產生器V3", description: "修改1張A4可以產生2張桌牌，並記錄座標定位，改為直接開啟PDF，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器03.html" },
+    { name: "A4三角桌牌產生器V4", description: "修改V3版，增加背景圖像載入，及備註與姓名的顏色字體設定，版面略調，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器04.html" },
 
 ];
