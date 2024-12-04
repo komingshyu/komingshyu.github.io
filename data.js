079bc595ecb836b003f5f1e9b5228f05d5267d5e
komingshyu <komingshyu@yahoo.com.tw>
2024-12-04 17:45:15 +0800

    增加markdown編輯器

--- data.js
+++ data.js
@@ -1,11 +1,13 @@
 const linksData = [
+    { name: "A4三角桌牌產生器V3", description: "修改1張A4可以產生2張桌牌，並記錄座標定位，改為直接開啟PDF，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器03.html" },
+    { name: "A4三角桌牌產生器V4", description: "修改V3版，增加背景圖像載入，及備註與姓名的顏色字體設定，版面略調，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器04.html" },    
+    { name: "批次獎狀套印V1", description: "可載入A4獎狀圖像及得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "批次獎狀套印(150dpi)01.html" },
+    { name: "單張獎狀套印V1", description: "可載入A4獎狀圖像及輸入得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "單張獎狀套印(150dpi)01.html" }, 
+    { name: "圖像智慧拼接系統V1", description: "將連續拍攝的照片拼接成一張完整圖像，需要有30%-50%的重疊影像", link: "photo_stitching/index.html" },
+    { name: "圖片自動裁切程式V1", description: "將圖像依照橫向縱向數量自動分割，並可微調分割線", link: "photo_auto_cut01.html" },   
+    { name: "markdown簡易編輯器V1", description: "markdown簡易web編輯器，使用md語法編輯，即時網頁預覽編輯結果", link: "md_edit01.html" },
+    { name: "圖片文字辨識OCR測試", description: "使用Tesseract.js開源OCR設計，可選擇辨識繁體中文、簡體中文及英文，辨識效果有待改進", link: "OCR.html" },
     { name: "1A2B猜數字遊戲", description: "傳統1A2B猜數字遊戲", link: "1A2B猜數字遊戲.html" },
     { name: "2048遊戲", description: "好玩的二進位遊戲", link: "2048-game.html" },
-    { name: "圖像智慧拼接系統", description: "將連續拍攝的照片拼接成一張完整圖像，需要有30%-50%的重疊影像", link: "photo_stitching/index.html" },
-    { name: "圖片自動裁切程式", description: "將圖像依照橫向縱向數量自動分割，並可微調分割線", link: "photo_auto_cut.html" },
-    { name: "批次獎狀套印", description: "可載入A4獎狀圖像及得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "批次獎狀套印(150dpi).html" },
-    { name: "單張獎狀套印", description: "可載入A4獎狀圖像及輸入得獎資料,再網頁上調整位置大小,並轉出PDF列印(150dpi)", link: "單張獎狀套印(150dpi).html" },
-    { name: "A4三角桌牌產生器V3", description: "修改1張A4可以產生2張桌牌，並記錄座標定位，改為直接開啟PDF，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器03.html" },
-    { name: "A4三角桌牌產生器V4", description: "修改V3版，增加背景圖像載入，及備註與姓名的顏色字體設定，版面略調，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器04.html" },
 
 ];
