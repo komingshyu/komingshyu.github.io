72bf0e75ee6c8868460ececdcc98e3d2771338dc
komingshyu <komingshyu@yahoo.com.tw>
2025-01-04 23:08:41 +0800

    加入配課程式

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
 const linksData = [
+    { name: "國中教師配課網頁版V2.4", description: "利用網頁的拖曳功能來進行教師配課作業，配課資料都存在本地端電腦，可以備份已完成部分到其他電腦匯入使用", link: "class_table/index.html" },
+
     { name: "A4標籤貼紙產生器V1", description: "將原三角桌牌程式修改為標籤貼紙產生器，可以列印有、無邊界的標籤貼紙，改自屏東後庄國小黃老師原三角桌牌，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "A4標籤貼紙產生器V1.html" },
     { name: "A4三角桌牌產生器V3", description: "修改1張A4可以產生2張桌牌，並記錄座標定位，改為直接開啟PDF，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器03.html" },
     { name: "A4三角桌牌產生器V4", description: "修改V3版，增加背景圖像載入，及備註與姓名的顏色字體設定，版面略調，改自屏東後庄國小黃老師，<a href=https://puti001.github.io/teach_tools/Triangle_Table_Card_Generator2.html target=_blank>原網址</a>", link: "三角桌牌產生器04.html" },   
