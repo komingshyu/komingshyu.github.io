39144039ba91596cdb83101741c7520f134fb2ae
komingshyu <komingshyu@yahoo.com.tw>
2025-01-13 18:28:59 +0800

    Update data.js

--- data.js
+++ data.js
@@ -20,7 +20,7 @@ const linksData = [
     { name: "圖像智慧拼接系統V1", description: "將連續拍攝的照片拼接成一張完整圖像，需要有30%-50%的重疊影像", link: "photo_stitching/index.html" },
     { name: "圖片自動裁切程式V1", description: "將圖像依照橫向縱向數量自動分割，並可微調分割線", link: "photo_auto_cut01.html" },   
     { name: "markdown簡易編輯器V1", description: "markdown簡易web編輯器，使用md語法編輯，即時網頁預覽編輯結果", link: "md_edit01.html" },
-    { name: "JSON格式解析網頁", description: "JSON格式解析網頁，載入json檔案會貼上json文件，即時網頁預覽解析結果", link: "json_view.html" },
+    { name: "JSON格式解析網頁", description: "JSON格式解析網頁，載入json檔案會貼上json文件，即時網頁預覽解析結果", link: "json-viewer.html" },
     { name: "圖片文字辨識OCR測試", description: "使用Tesseract.js開源OCR設計，可選擇辨識繁體中文、簡體中文及英文，辨識效果有待改進", link: "OCR.html" },
     { name: "1A2B猜數字遊戲", description: "傳統1A2B猜數字遊戲", link: "1A2B猜數字遊戲.html" },
     { name: "2048遊戲", description: "好玩的二進位遊戲", link: "2048-game.html" },
