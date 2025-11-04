c9f0c1bd5439a806495b363b8a43a0ac6f0f8da0
komingshyu <komingshyu@yahoo.com.tw>
2025-11-04 10:22:44 +0800

    增加教學資料

---
+++ teach/data.js
@@ -0,0 +1,7 @@
+const linksData = [
+  { name: "二進位太空探險", description: "二進位運算遊戲", link: "二進位太空探險.html" },
+
+  { name: "聲音數位化 取樣", description: "載入音訊檔，藉由不同取樣來比較了解取樣的用途", link: "聲音取樣教學實驗室.html" },
+  { name: "向量圖與點陣圖的比較", description: "藉由向量圖與點陣圖放大比較了解差異", link: "向量圖 vs 點陣圖 - 互動教學工具.html" },
+
+];
