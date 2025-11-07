ba7f66da93baf42a3365a252eef01771b1815c3a
komingshyu <komingshyu@yahoo.com.tw>
2025-11-07 11:27:42 +0800

    加入圖像教學

--- teach/data.js
+++ teach/data.js
@@ -1,7 +1,10 @@
 const linksData = [
+  { name: "玩遊戲學二進位(易)", description: "CISCO推出二進位學習遊戲", link: "https://learningcontent.cisco.com/games/binary/index.html" },
+  { name: "玩遊戲學二進位(中等)", description: "另外一個網路上的二進位好玩遊戲", link: "https://games.penjee.com/binary-bonanza/" },
   { name: "二進位太空探險", description: "二進位運算遊戲", link: "二進位太空探險.html" },
-
   { name: "聲音數位化 取樣", description: "載入音訊檔，藉由不同取樣來比較了解取樣的用途", link: "聲音取樣教學實驗室.html" },
   { name: "向量圖與點陣圖的比較", description: "藉由向量圖與點陣圖放大比較了解差異", link: "向量圖 vs 點陣圖 - 互動教學工具.html" },
+  { name: "點陣圖像位元深度比較教學工具", description: "點陣圖像位元深度比較", link: "點陣圖像位元深度比較教學工具.html" },
+  { name: "圖像取樣教學工具", description: "圖像取樣的比較工具", link: "圖像取樣教學工具.html" },
 
 ];
