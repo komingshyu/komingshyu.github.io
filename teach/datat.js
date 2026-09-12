ed1545c63bd820180ce9fcf3590bd955052b8bad
komingshyu <komingshyu@yahoo.com.tw>
2026-09-12 20:51:45 +0800

    河內之塔遊戲

--- teach/datat.js
+++ teach/datat.js
@@ -9,6 +9,7 @@ const linksData = [
   { name: "RGB色彩疊合實驗室", description: "RGB色彩疊合實驗室", link: "RGB色彩疊合實驗室.html" },
   { name: "RGB三原色光互動教學 - 色光照射模擬", description: "RGB三原色光互動教學", link: "RGB三原色光互動教學 - 色光照射模擬.html" },
   { name: "國中資訊科技系統平台認識", description: "國中資訊科技系統平台認識與模擬網頁", link: "國中資訊科技系統平台認識.html" },
-  { name: "智慧流程圖產生器V2", description: "智慧流程圖產生器V2", link: "智慧流程圖產生器V2.html" }
+  { name: "智慧流程圖產生器V2", description: "智慧流程圖產生器V2", link: "智慧流程圖產生器V2.html" },
+   { name: "河內之塔遊戲", description: "河內之塔遊戲", link: "河內之塔遊戲.html" }
 
 ];
